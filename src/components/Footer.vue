8a5fa5b4168698971fda31f95c4d3c39e25aea3d
Amr Monier <a.monier2107@gmail.com>
2020-02-29 17:16:14 +0200

    initiated Fire base and added the footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="page-footer indigo darken-2">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text"> Maniac Smoothies</h5>
+                <p class="grey-text text-lighten-4">Is a VueJs Project devrloped as a practice on the framework</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Contact with me</h5>
+                <ul>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="https://github.com/AmrMonier">
+                            Github account: AmrMonier
+                        </a>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="mailto:a.monier2107@gmail.com">
+                            Gmail: a.monier2107@gmail.com
+                        </a>
+                    </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright Reserved to Amr Monier
+            </div>
+          </div>
+        </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import CreateSmoothie from '@/components/CreateSmoothie'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
